b5089353bad6c051b37714f92b8640614a35d243
pasule <3086874696@qq.com>
2025-02-14 23:51:09 +0800

    前端代码

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app" style="max-width: 1240px;margin: 0 auto"> class="">
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "app",
+};
+</script>
+
+<style lang="scss">
+*{
+  padding: 0;
+  margin:0;
+}
+html,body{
+  width: 100%;
+  height: 100%;
+}
+#app{
+  height:100%;
+}
+body {
+  padding: 0;
+  margin: 0;
+  
+}
+</style>
